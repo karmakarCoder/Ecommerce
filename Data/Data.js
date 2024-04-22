a5644ccbf5ac159850c1f6b74fa0aa6e6c149c35
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-04-22 12:04:37 +0600

    color categories data

---
+++ Data/Data.js
@@ -0,0 +1,51 @@
+export const categories = [
+  {
+    id: 1,
+    title: "Health & Beauty",
+    subcategories: [
+      { id: 1, title: "Skin Care" },
+      { id: 2, title: "Hare Care" },
+      { id: 3, title: "Makeup" },
+      { id: 4, title: "Beauty Tools" },
+      { id: 5, title: "Men's Care" },
+    ],
+  },
+  {
+    id: 2,
+    title: "Men's & Boys Fashion",
+    subcategories: [],
+  },
+  {
+    id: 3,
+    title: "Electronics Device",
+    subcategories: [
+      { id: 1, title: "Smartphone" },
+      { id: 2, title: "Tablets" },
+      { id: 3, title: "Cameras" },
+      { id: 4, title: "Laptop" },
+      { id: 5, title: "Desktop" },
+    ],
+  },
+  {
+    id: 4,
+    title: "Groceries",
+    subcategories: [],
+  },
+  {
+    id: 5,
+    title: "Sports & Outdoors",
+    subcategories: [
+      { id: 1, title: "Exercise & Fitness" },
+      { id: 2, title: "Treadmills" },
+      { id: 3, title: "Cycling" },
+    ],
+  },
+];
+
+export const colorCategoriesData = [
+  { id: 1, title: "Color 1", colorCode: "#000000" },
+  { id: 2, title: "Color 1", colorCode: "#FF8686" },
+  { id: 3, title: "Color 1", colorCode: "#7ED321" },
+  { id: 4, title: "Color 1", colorCode: "#B6B6B6" },
+  { id: 5, title: "Color 1", colorCode: "#15CBA5" },
+];
