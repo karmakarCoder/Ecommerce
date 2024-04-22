c064991ed1320aa17aa1b6d39e80d23300aba86d
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-04-22 23:46:29 +0600

    modified

--- Data/Data.js
+++ Data/Data.js
@@ -49,3 +49,135 @@ export const colorCategoriesData = [
   { id: 4, title: "Color 1", colorCode: "#B6B6B6" },
   { id: 5, title: "Color 1", colorCode: "#15CBA5" },
 ];
+
+import product1 from "../src/assets/product1.png";
+import product2 from "../src/assets/product2.png";
+import product3 from "../src/assets/product3.png";
+import product4 from "../src/assets/product4.png";
+
+export const newProductData = [
+  {
+    id: 1,
+    title: "Mini Table Clck",
+    img: product1,
+    price: "$15.00",
+    discount: true,
+    discountStatus: "",
+    colorVariant: true,
+  },
+  {
+    id: 2,
+    title: "Latest Smart Watch",
+    img: product2,
+    price: "$35.00",
+    discount: true,
+    discountStatus: "5%",
+    colorVariant: false,
+  },
+  {
+    id: 3,
+    title: "Bucket",
+    img: product3,
+    price: "$20.00",
+    discount: true,
+    discountStatus: "10%",
+    colorVariant: true,
+  },
+  {
+    id: 4,
+    title: "Children Bag",
+    img: product4,
+    price: "$60.00",
+    discount: true,
+    discountStatus: "New",
+    colorVariant: false,
+  },
+];
+
+import product5 from "../src/assets/product5.png";
+import product6 from "../src/assets/product6.png";
+import product7 from "../src/assets/product7.png";
+import product8 from "../src/assets/product8.png";
+
+export const bestSellerData = [
+  {
+    id: 1,
+    title: "Mini Table Clck",
+    img: product5,
+    price: "$15.00",
+    discount: true,
+    discountStatus: "",
+    colorVariant: true,
+  },
+  {
+    id: 2,
+    title: "Latest Smart Watch",
+    img: product6,
+    price: "$35.00",
+    discount: true,
+    discountStatus: "5%",
+    colorVariant: false,
+  },
+  {
+    id: 3,
+    title: "Bucket",
+    img: product7,
+    price: "$20.00",
+    discount: true,
+    discountStatus: "10%",
+    colorVariant: true,
+  },
+  {
+    id: 4,
+    title: "Children Bag",
+    img: product8,
+    price: "$60.00",
+    discount: true,
+    discountStatus: "New",
+    colorVariant: false,
+  },
+];
+
+import product9 from "../src/assets/product9.png";
+import product10 from "../src/assets/product10.png";
+import product11 from "../src/assets/product11.png";
+import product12 from "../src/assets/product12.png";
+
+export const specialOfferData = [
+  {
+    id: 1,
+    title: "Mini Table Clck",
+    img: product9,
+    price: "$15.00",
+    discount: true,
+    discountStatus: "",
+    colorVariant: true,
+  },
+  {
+    id: 2,
+    title: "Latest Smart Watch",
+    img: product10,
+    price: "$35.00",
+    discount: true,
+    discountStatus: "5%",
+    colorVariant: false,
+  },
+  {
+    id: 3,
+    title: "Bucket",
+    img: product11,
+    price: "$20.00",
+    discount: true,
+    discountStatus: "10%",
+    colorVariant: true,
+  },
+  {
+    id: 4,
+    title: "Children Bag",
+    img: product12,
+    price: "$60.00",
+    discount: true,
+    discountStatus: "New",
+    colorVariant: false,
+  },
+];
