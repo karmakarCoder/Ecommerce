0bae131bbbc15300ed42b868323bcc5024131606
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-04-22 23:48:46 +0600

    modified

--- src/Common/AdsOverlay.jsx
+++ src/Common/AdsOverlay.jsx
@@ -8,7 +8,7 @@ const AdsOverlay = ({ className, title, discount, tagline }) => {
         <h2 className="text-3xl sm:text-4xl font-bold font-DMsans text-primaryFontColor pb-2 sm:pb-9 md:pb-3">
           {title ? title : "Sale"}
         </h2>
-        <p className="whitespace-nowrap text-sm sm:text-base font-DMsans font-normal text-thirdFontColor pb-2 sm:pb-12">
+        <p className="whitespace-nowrap text-sm sm:text-base font-DMsans font-normal text-thirdFontColor pb-2 sm:pb-5">
           Up to{" "}
           <span className="font-bold text-primaryFontColor text-3xl sm:text-4xl align-middle px-1 sm:px-2">
             {discount ? discount : "00%"}
