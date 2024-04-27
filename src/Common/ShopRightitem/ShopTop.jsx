f496ccace0221465d152a9c3c9f5c0fe7a207e6b
Rodro-Kumar <dev.rodro@gmail.com>
2024-04-28 00:40:42 +0600

    modified

--- src/Common/ShopRightitem/ShopTop.jsx
+++ src/Common/ShopRightitem/ShopTop.jsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import { HiMiniSquares2X2 } from "react-icons/hi2";
 import { MdArrowDropUp } from "react-icons/md";
 import { Link } from "react-router-dom";
-import { Feature } from "../../../Data/Data";
+import { Feature , pageShow} from "../../../Data/Data";
 
 const ShopTop = () => {
   const [feature, setfeature] = useState(false);
+  const [show, setshow] = useState(false);
   const [featureItem, setfeatureItem] = useState(false);
+  const [Showitem, setShowitem] = useState(false);
 
   const HandleItem = (event) => {
     setfeatureItem(event.target.innerText);
@@ -15,6 +17,14 @@ const ShopTop = () => {
 
   const HandleFeature = () => {
     setfeature(!feature);
+  };
+
+  const HandleShow = () => {
+    setshow(!show)
+  }
+
+  const HandleShowItem = (e) => {
+    setShowitem(e.target.innerText)
   };
   return (
     <>
@@ -58,6 +68,42 @@ const ShopTop = () => {
               ))}
             </div>
           ) : null}
+        </div>
+        <div>
+          <h4 className="text-base font-DMsans font-normal text-primaryFontColor">Show</h4>
+          <div>
+          <div className="relative py-1 px-5 border-2 border-[#F0F0F0] w-[239px]">
+          <div
+            className="flex items-center justify-between cursor-pointer"
+            onClick={HandleShow}
+          >
+            <p className="text-base font-DMsans font-normal text-secondaryFontColor">
+              {Showitem ? Showitem : "9"}
+            </p>
+            <span>
+              <MdArrowDropUp
+                className={`text-2xl text-secondaryFontColor ${
+                  show
+                    ? "rotate-[0] transition-all duration-300"
+                    : "rotate-[180deg] transition-all duration-300"
+                }`}
+              />
+            </span>
+          </div>
+          {feature ? (
+            <div className="bg-[#F0F0F0] border-2 border-[#0000001f] w-full shadow-md absolute top-[40px] left-0 flex flex-col gap-y-1 rounded">
+              {pageShow.map((item) => (
+                <div
+                  className="cursor-pointer active:bg-[#d3ac7138] hover:bg-white text-primaryFontColor text-md pl-2 font-DMsans font-medium py-2 "
+                  onClick={HandleShowItem}
+                >
+                  <Link>{item.title}</Link>
+                </div>
+              ))}
+            </div>
+          ) : null}
+        </div>
+          </div>
         </div>
       </div>
     </>
