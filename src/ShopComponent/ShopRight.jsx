e97acc417f908eef09af6348f7dd59fa41f0b6fa
Rodro-Kumar <dev.rodro@gmail.com>
2024-04-30 12:32:01 +0600

    modified

--- src/ShopComponent/ShopRight.jsx
+++ src/ShopComponent/ShopRight.jsx
@@ -1,20 +1,19 @@
 import React, { useEffect } from "react";
-import axios from "axios";
+
 import ShopTop from "../Common/ShopRightitem/ShopTop";
+import ShopBottom from "../Common/ShopRightitem/ShopBottom";
 
 const ShopRight = ({ className }) => {
-  useEffect(() => {
-    const Datafetcher = async () => {
-      // const data = await axios.get("https://dummyjson.com/products");
-      // console.log(data.data.products);
-    };
-    Datafetcher();
-  }, []);
+
   return (
     <>
       <div className={className}>
         <div>
           <ShopTop />
+        </div>
+
+        <div>
+          <ShopBottom/>
         </div>
       </div>
     </>
