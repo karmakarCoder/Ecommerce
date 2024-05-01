c05683c6fd8820e6330e3118d9186f3910d43069
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-05-01 16:34:25 +0600

    modified

--- src/Common/ShopRightitem/ShopTop.jsx
+++ src/Common/ShopRightitem/ShopTop.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { HiMiniSquares2X2 } from "react-icons/hi2";
 import { MdArrowDropUp } from "react-icons/md";
 import { Link } from "react-router-dom";
-import { Feature , pageShow} from "../../../Data/Data";
+import { Feature, pageShow } from "../../../Data/Data";
 
 const ShopTop = () => {
   const [feature, setfeature] = useState(false);
@@ -20,90 +20,96 @@ const ShopTop = () => {
   };
 
   const HandleShow = () => {
-    setshow(!show)
-  }
+    setshow(!show);
+  };
 
   const HandleShowItem = (e) => {
-    setShowitem(e.target.innerText)
-    setshow(false)
+    setShowitem(e.target.innerText);
+    setshow(false);
   };
   return (
     <>
-      <div className="pb-[60px] flex items-center justify-end gap-x-4">
-        <div><span className="cursor-pointer">
+      <div className="pb-[60px] flex items-center justify-between gap-x-4">
+        <div>
+          <span className="cursor-pointer text-xl">
             <HiMiniSquares2X2 />
-          </span></div>
-        <div className="flex items-center gap-x-2">
-          <p className="text-base font-DMsans font-normal text-secondaryFontColor">
-            Sort by:
-          </p>
-          
+          </span>
         </div>
-        <div className="relative py-1 px-5 border-2 border-[#F0F0F0] w-[239px]">
-          <div
-            className="flex items-center justify-between cursor-pointer"
-            onClick={HandleFeature}
-          >
+        <div className="flex items-center gap-x-5">
+          <div className="flex items-center gap-x-2">
             <p className="text-base font-DMsans font-normal text-secondaryFontColor">
-              {featureItem ? featureItem : "Featured"}
+              Sort by:
             </p>
-            <span>
-              <MdArrowDropUp
-                className={`text-2xl text-secondaryFontColor ${
-                  feature
-                    ? "rotate-[0] transition-all duration-300"
-                    : "rotate-[180deg] transition-all duration-300"
-                }`}
-              />
-            </span>
           </div>
-          {feature ? (
-            <div className="bg-[#F0F0F0] border-2 border-[#0000001f] w-full shadow-md absolute top-[40px] left-0 flex flex-col gap-y-1 rounded">
-              {Feature.map((item) => (
-                <div
-                  className="cursor-pointer active:bg-[#d3ac7138] hover:bg-white text-primaryFontColor text-md pl-2 font-DMsans font-medium py-2 "
-                  onClick={HandleItem}
-                >
-                  <Link>{item.title}</Link>
-                </div>
-              ))}
+          <div className="relative py-1 px-5 border-2 border-[#F0F0F0] w-[239px]">
+            <div
+              className="flex items-center justify-between cursor-pointer"
+              onClick={HandleFeature}
+            >
+              <p className="text-base font-DMsans font-normal text-secondaryFontColor">
+                {featureItem ? featureItem : "Featured"}
+              </p>
+              <span>
+                <MdArrowDropUp
+                  className={`text-2xl text-secondaryFontColor ${
+                    feature
+                      ? "rotate-[0] transition-all duration-300"
+                      : "rotate-[180deg] transition-all duration-300"
+                  }`}
+                />
+              </span>
             </div>
-          ) : null}
-        </div>
-        <div className="flex items-center gap-x-2">
-          <h4 className="text-base font-DMsans font-normal text-primaryFontColor">Show</h4>
-          <div>
-          <div className="relative py-1 px-4 border-2 border-[#F0F0F0] w-[140px]">
-          <div
-            className="flex items-center justify-between cursor-pointer"
-            onClick={HandleShow}
-          >
-            <p className="text-base font-DMsans font-normal text-secondaryFontColor">
-              {Showitem ? Showitem : "9"}
-            </p>
-            <span>
-              <MdArrowDropUp
-                className={`text-2xl text-secondaryFontColor ${
-                  show
-                    ? "rotate-[0] transition-all duration-300"
-                    : "rotate-[180deg] transition-all duration-300"
-                }`}
-              />
-            </span>
+            {feature ? (
+              <div className="z-50 bg-[#F0F0F0] border-2 border-[#0000001f] w-full shadow-md absolute top-[40px] left-0 flex flex-col gap-y-1 rounded">
+                {Feature.map((item) => (
+                  <div
+                    className="cursor-pointer active:bg-[#d3ac7138] hover:bg-white text-primaryFontColor text-md pl-2 font-DMsans font-medium py-2 "
+                    onClick={HandleItem}
+                  >
+                    <Link>{item.title}</Link>
+                  </div>
+                ))}
+              </div>
+            ) : null}
           </div>
-          {show ? (
-            <div className="bg-[#F0F0F0] border-2 border-[#0000001f] w-full shadow-md absolute top-[40px] left-0 flex flex-col gap-y-1 rounded">
-              {pageShow.map((item) => (
+          <div className="flex items-center gap-x-2">
+            <h4 className="text-base font-DMsans font-normal text-primaryFontColor">
+              Show
+            </h4>
+            <div>
+              <div className="relative py-1 px-4 border-2 border-[#F0F0F0] w-[140px]">
                 <div
-                  className="cursor-pointer active:bg-[#d3ac7138] hover:bg-white text-primaryFontColor text-md pl-2 font-DMsans font-medium py-2 "
-                  onClick={HandleShowItem}
+                  className="flex items-center justify-between cursor-pointer"
+                  onClick={HandleShow}
                 >
-                  <Link>{item.title}</Link>
+                  <p className="text-base font-DMsans font-normal text-secondaryFontColor">
+                    {Showitem ? Showitem : "9"}
+                  </p>
+                  <span>
+                    <MdArrowDropUp
+                      className={`text-2xl text-secondaryFontColor ${
+                        show
+                          ? "rotate-[0] transition-all duration-300"
+                          : "rotate-[180deg] transition-all duration-300"
+                      }`}
+                    />
+                  </span>
                 </div>
-              ))}
+                {show ? (
+                  <div className="z-50 bg-[#F0F0F0] border-2 border-[#0000001f] w-full shadow-md absolute top-[40px] left-0 flex flex-col gap-y-1 rounded">
+                    {pageShow.map((item) => (
+                      <div
+                        key={item.id}
+                        className="cursor-pointer active:bg-[#d3ac7138] hover:bg-white text-primaryFontColor text-md pl-2 font-DMsans font-medium py-2 "
+                        onClick={HandleShowItem}
+                      >
+                        <Link>{item.title}</Link>
+                      </div>
+                    ))}
+                  </div>
+                ) : null}
+              </div>
             </div>
-          ) : null}
-        </div>
           </div>
         </div>
       </div>
