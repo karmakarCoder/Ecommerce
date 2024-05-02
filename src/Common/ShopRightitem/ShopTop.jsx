d6ea7fa6b60e5628bd01b4eb3a5306238dce3693
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-05-02 12:00:16 +0600

    modified

--- src/Common/ShopRightitem/ShopTop.jsx
+++ src/Common/ShopRightitem/ShopTop.jsx
@@ -17,10 +17,12 @@ const ShopTop = () => {
 
   const HandleFeature = () => {
     setfeature(!feature);
+    setshow(false);
   };
 
   const HandleShow = () => {
     setshow(!show);
+    setfeature(false);
   };
 
   const HandleShowItem = (e) => {
