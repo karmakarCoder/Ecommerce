a758d851539d19d618c5807d8a68a33eaf3dec0f
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-05-03 11:01:10 +0600

    modified

--- src/ShopComponent/ShopRight.jsx
+++ src/ShopComponent/ShopRight.jsx
@@ -1,20 +1,24 @@
-import React, { useEffect } from "react";
-
+import React, { createContext, useState } from "react";
 import ShopTop from "../Common/ShopRightitem/ShopTop";
 import ShopBottom from "../Common/ShopRightitem/ShopBottom";
+export const shopRightPageContext = createContext();
 
 const ShopRight = ({ className }) => {
+  const [showPage, setshowPage] = useState(9);
 
+  const HandlePagination = (event) => {
+    setshowPage(event.target.innerText);
+  };
   return (
     <>
       <div className={className}>
         <div>
-          <ShopTop />
+          <ShopTop pageValue={HandlePagination} />
         </div>
 
-        <div>
-          <ShopBottom/>
-        </div>
+        <shopRightPageContext.Provider value={showPage}>
+          <ShopBottom />
+        </shopRightPageContext.Provider>
       </div>
     </>
   );
