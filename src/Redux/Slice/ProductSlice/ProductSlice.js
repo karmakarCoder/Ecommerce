997e66cd922c42b62c4fdf81b3743e6e025c820b
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-05-05 19:53:12 +0600

    redux thunk

--- src/Redux/Slice/ProductSlice/ProductSlice.js
+++ src/Redux/Slice/ProductSlice/ProductSlice.js
@@ -1,8 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const apiStatus = {
+  IDLE: "IDLE",
+  LOADING: "LOADING",
+  ERROR: "ERROR",
+};
+
 const initialState = {
   data: [],
-  status: "idle",
+  status: apiStatus.IDLE,
 };
 
 export const ProductSlice = createSlice({
@@ -12,10 +18,31 @@ export const ProductSlice = createSlice({
     setProduct: (state, payload) => {
       state.data = payload;
     },
+    setStatus: (state, payload) => {
+      state.status = payload;
+    },
   },
 });
 
+// thunk function for get data
+
+export const fetchData = () => {
+  return async function getData(dispatch, getState) {
+    try {
+      dispatch(setStatus(apiStatus.LOADING));
+      const response = await fetch("https://dummyjson.com/products");
+      const data = await response.json();
+      const allProduct = await data.products;
+      dispatch(setProduct(allProduct));
+      dispatch(setStatus(apiStatus.IDLE));
+    } catch (error) {
+      dispatch(setStatus(apiStatus.ERROR));
+      console.log(error);
+    }
+  };
+};
+
 // Action creators are generated for each case reducer function
-export const { setProduct } = ProductSlice.actions;
+export const { setProduct, setStatus } = ProductSlice.actions;
 
 export default ProductSlice.reducer;
