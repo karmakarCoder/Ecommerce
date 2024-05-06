82874f2cc0c6332c97840016e488439fc4e50fe5
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-05-06 11:02:07 +0600

    loading product card data

--- Data/Data.js
+++ Data/Data.js
@@ -236,3 +236,33 @@ export const pageShow = [
     title: "27",
   },
 ];
+
+export const loadingProductData = [
+  {
+    id: 1,
+  },
+  {
+    id: 2,
+  },
+  {
+    id: 3,
+  },
+  {
+    id: 4,
+  },
+  {
+    id: 5,
+  },
+  {
+    id: 6,
+  },
+  {
+    id: 7,
+  },
+  {
+    id: 8,
+  },
+  {
+    id: 9,
+  },
+];
