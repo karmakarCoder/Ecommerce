769e390e7f835e4f5de65fd00de23516c73db9e0
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-05-10 11:31:30 +0600

    responsive update

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -8,16 +8,16 @@ const Shop = () => {
     <>
       <div className="px-4">
         <div className="container">
-          <div className="py-32">
-            <h1 className="text-5xl font-bold text-primaryFontColor pb-3">
+          <div className="py-9 md:py-32">
+            <h1 className="text-4xl md:text-5xl font-bold text-primaryFontColor pb-3">
               Products
             </h1>
             <BreadCrumb />
           </div>
 
-          <div className="flex py-16 gap-x-10 flex-col lg:flex-row">
-            <Shopleft className={"w-[100%] lg:w-[25%]"} />
-            <ShopRight className={"w-[75%]"} />
+          <div className="flex py-16 gap-x-10 flex-col md:flex-row justify-between">
+            <Shopleft className={"w-[100%] md:w-[30%] lg:w-[25%]"} />
+            <ShopRight className={"w-[100%] md:w-[70%] lg:w-[75%]"} />
           </div>
         </div>
       </div>
