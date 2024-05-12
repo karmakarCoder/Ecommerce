98b6d51c52450c2fce0c389283ea94bcf483f8c6
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-05-12 10:26:57 +0600

    product details page

---
+++ src/Pages/Products/Products.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import BreadCrumb from "../../Common/BreadCrumb/BreadCrumb";
+import { fetchData } from "../../Redux/Slice/ProductSlice/ProductSlice";
+import { useSelector, useDispatch } from "react-redux";
+
+const Products = () => {
+  const dispatch = useDispatch();
+  const [EachProduct, setEachProduct] = useState();
+
+  console.log(EachProduct);
+
+  useEffect(() => {
+    dispatch(fetchData("https://dummyjson.com/products/1"));
+  }, []);
+
+  const { data, status } = useSelector((state) => state.allProduct);
+
+  useEffect(() => {
+    if (status.payload === "IDLE") {
+      setEachProduct(data.payload);
+    }
+  }, [status.payload, data.payload]);
+
+  return (
+    <>
+      <div>
+        <div className="container">
+          <div>
+            <BreadCrumb />
+          </div>
+
+          <div>
+            <picture>
+              <img src="" alt="" />
+            </picture>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Products;
