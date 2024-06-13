1c196b84aa1a86b12ca970087f5f7e2738b08e76
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-06-13 10:15:32 +0600

    wish list redux slice

---
+++ src/Redux/AddtoWishSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { toast, Bounce } from "react-toastify";
+
+const ListStatus = {
+  IDLE: "IDLE",
+  LOADING: "LOADING",
+  ERROR: "ERROR",
+};
+
+const initialState = {
+  wishListData: [],
+  status: ListStatus.IDLE,
+};
+
+const wishSlice = createSlice({
+  name: "wish",
+  initialState,
+  reducers: {
+    addToWishList: (state, action) => {
+      state.status = ListStatus.LOADING;
+      state.wishListData = action.payload;
+      state.status = ListStatus.IDLE;
+      toast("add", {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
+    },
+  },
+});
+
+export const { addToWishList } = wishSlice.actions;
+
+export default wishSlice.reducer;
