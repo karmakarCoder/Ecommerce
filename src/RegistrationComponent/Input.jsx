0fa6c4b46a400de1ec17dddaf7182bf535bcc950
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-06-20 23:40:23 +0600

    modified

--- src/RegistrationComponent/Input.jsx
+++ src/RegistrationComponent/Input.jsx
@@ -15,13 +15,14 @@ const Input = ({
       <p className="text-xs md:text-base font-DMsans font-bold text-primaryFontColor pb-2">
         {inputTitle ? inputTitle : "Title"}
       </p>
+
       <input
         type={inputType}
         id={inputId}
         value={inputValue}
         name={inputName}
         placeholder={placeHolder ? placeHolder : "Placeholder"}
-        className={`placeholder:md:text-sm placeholder:text-xs placeholder:font-DMsans placeholder:text-secondaryFontColor placeholder:font-normal border-b-2  pb-4 w-[90%] focus:border-yellow-400 ${className}`}
+        className={`placeholder:md:text-sm placeholder:text-xs placeholder:font-DMsans placeholder:text-secondaryFontColor placeholder:font-normal border-b-2 pb-4 w-[90%] focus:border-yellow-400 ${className}`}
         onChange={onChangeInput}
       />
     </div>
